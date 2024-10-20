c29ec8590868dc9ebf0a213ed4e659f8c49339d1
isthismyniche <manishnair999@gmail.com>
2024-10-20 14:14:06 +0800

    first_commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import streamlit as st  
+from helper_functions.utility import check_password  
+
+# Check if the password is correct.  
+if not check_password():  
+    st.stop()
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="My Streamlit App"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("Streamlit App")
+
+form = st.form(key="form")
+form.subheader("Prompt")
+
+user_prompt = form.text_area("Enter your prompt here", height=200)
+
+st.write(user_prompt)
